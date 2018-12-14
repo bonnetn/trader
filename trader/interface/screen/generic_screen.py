dfaf7e199140acee90c9066ded552996a107ce43
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-12-14 16:14:32 +0100

    Add a method to change planet

--- trader/interface/screen/generic_screen.py
+++ trader/interface/screen/generic_screen.py
@@ -1,8 +1,9 @@
 """
 All is a module that contains misc. function that can be called from everywhere.
 """
-from trader.interface.constants import ALL_RESOURCES, METAL, CRYSTAL, DEUTERIUM, ENERGY, TIMEOUT
 from trader.interface.base_screen import Screen
+from trader.interface.constants import ALL_RESOURCES, METAL, CRYSTAL, DEUTERIUM, ENERGY, TIMEOUT
+from trader.util.log import LOG
 
 XPATH_COUNTER = {
     METAL: '//*[@id="resources_metal"]',
@@ -10,6 +11,13 @@ XPATH_COUNTER = {
     DEUTERIUM: '//*[@id="resources_deuterium"]',
     ENERGY: '//*[@id="resources_energy"]',
 }
+
+PLANETS = '//*[@id="planetList"]'
+SMALL_PLANET_XPATH = '//*[contains(@class,"smallplanet")]'
+
+from collections import namedtuple
+
+Planet = namedtuple('Planet', ['coord', 'name'])
 
 
 class GenericScreen(Screen):
@@ -19,6 +27,22 @@ class GenericScreen(Screen):
     def move(self) -> None:
         raise NotImplementedError()
 
+    def select_planet(self, target) -> None:
+        planets = self.driver.find_elements_by_xpath(SMALL_PLANET_XPATH)
+        for planet in planets:
+            planet_coords = planet.find_element_by_class_name("planet-koords").text
+            if planet_coords == target.coord:
+                planet_name = planet.find_element_by_class_name("planet-name").text
+                if planet_name == target.name:
+                    LOG.debug("Selecting planet for {}".format(planet))
+                    planet.find_element_by_xpath('.//a[contains(@class, "planetlink")]').click()
+                else:
+                    LOG.debug("Selecting moon for {}".format(planet))
+                    planet.find_element_by_xpath('.//a[contains(@class, "moonlink")]').click()
+
+                return
+
+        raise Exception("Could not find planet {}".format(target))
     def extract_resources(self):
         """
         Extract the resource count from the top band.
